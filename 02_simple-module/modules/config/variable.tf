6890cf4bf23c43e4c1a7b7d38f7ac6ea9376c099
Rolph de Ruiter <768145+RolphR@users.noreply.github.com>
2024-03-18 20:57:52 +0100

    Added 02_simple_module

---
+++ 02_simple-module/modules/config/variable.tf
@@ -0,0 +1,11 @@
+variable "config" {
+  type = object({
+    name        = string
+    some_number = optional(number, 42)
+    nested = object({
+      key   = string
+      value = number
+    })
+    feature_flags = map(bool)
+  })
+}
